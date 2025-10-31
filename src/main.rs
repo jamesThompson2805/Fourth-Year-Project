96650026d3702617efb71cdacf763bf583a446f9
jamesThompson2805 <jamesthompson2805@gmail.com>
2025-10-31 12:17:15 +0000

    Merged main adaptation with current issues

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,36 @@ mod parsing;
 mod transformations;
 
 use std::fs;
+use std::fs::File;
+use std::io::{Error, Write};
 
 use crate::{straight_line_program::stringify_slp, transformations::transform_x_i_program};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn save_input_to_output(vi: &Vec::<u32>,out_name: String) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string("input.txt")
+        .expect("Should have been able to read the file");
+
+    let res = parsing::file_parser(&contents);
+
+   match res {
+    Ok(slp) => {
+        println!("gathered output: {slp:?}");
+        println!();
+        let maybe_slp = transform_x_i_program(&slp, &vec![1,0,2,0,0,0,0,0,0]);
+        if let Some(res_slp) = maybe_slp {
+            println!("Success, received SLP");
+            let path = out_name+".txt";
+            let mut output = File::create(path)?;
+            write!(output, "===== INPUT SLP =====\n{}\n===== TRANSFORM =====\n{:?}\n===== OUTPUT SLP =====\n{}", stringify_slp(&slp), vi, stringify_slp(&res_slp))?;
+        }
+    },
+
+    Err(s) => println!("ERROR IN PARSING: \n{s}"),
+   } 
+   Ok(())
+}
+
+fn print_input_to_output(vi: &Vec::<u32>,out_name: &str) -> Result<(), Box<dyn std::error::Error>> {
     let contents = fs::read_to_string("input.txt")
         .expect("Should have been able to read the file");
 
@@ -26,4 +52,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
    } 
 
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let vi = vec![1,0,2,0,0,0,0,0,0];
+    save_input_to_output(&vi, "example_single_multiplication".to_string())
 }
