78be6cece0e20c5b6db7ef9c954572e4b813d22a
jamesThompson2805 <jamesthompson2805@gmail.com>
2026-01-16 15:57:01 +0000

    Implemented a preliminary attempt to properly project, appears errors in slp projection

--- src/projections.rs
+++ src/projections.rs
@@ -55,11 +55,10 @@ fn casimir_eigenval(casimir_num: u32, lambda: &Vec<u32>, k: usize) -> i64 {
             0
         }
     });
-    dbg!(&a);
-    let a_pow_p = dbg!(a.pow(casimir_num));
+    let a_pow_p = a.pow(casimir_num);
     let e = DMatrix::<i64>::from_element(k, k, 1);
 
-    dbg!(a_pow_p * e).trace()
+    (a_pow_p * e).trace()
 }
 
 /// find_distinguishing_casimir_index finds the index of a casimir element that has a different eigenvalue for the two partitions provided
@@ -152,7 +151,7 @@ fn partitions_n_le_m( n: usize, m: usize) -> Vec<Vec<u32>> {
 fn apply_casimir_to_slp<T,F>(slp: &SLP<T>, casimir_index: usize, casimir_size: usize, u32_to_c: F) -> Result<SLP<T>, String>
 where T: Clone + Display, F: Fn(u32) -> T,
 {
-    let casimir = dbg!(find_casimir(casimir_index, casimir_size));
+    let casimir = find_casimir(casimir_index, casimir_size);
 
     let mut mu_c_slp_summands: LASum<SLP<T>> = vec![];
     for eij_prod in casimir.iter() {
@@ -161,7 +160,7 @@ where T: Clone + Display, F: Fn(u32) -> T,
             Ok(transform_slp) => mu_c_slp_summands.push(transform_slp),
             Err(s) => return Err( format!("Failure at casimir {casimir:?}, error{s}, SLP:\n{}", stringify_slp(&slp))),
         }
-        println!("Transformed slp: \n{}", stringify_slp(mu_c_slp_summands.last().unwrap()));
+        //println!("Transformed slp: \n{}", stringify_slp(mu_c_slp_summands.last().unwrap()));
     }
     if mu_c_slp_summands.len() == 0 {
         return Err(String::from("casimir element empty, invalid index given"));
@@ -178,6 +177,7 @@ use std::fmt::Debug;
 use std::ops::{Add, Sub, Mul, Div};
 
 
+/// Will not worK! (multiplication doesn't work)
 /// apply_candidate_partitions_to_slp applies all candidate
 /// the projection is defined as P_{\lambda} = \Pi_{\mu \in \Lambda, \mu \neq \lambda} {
 ///     \frac{ U_{\mu} - \Chi_{\mu}(U_{\mu}) }{ \Chi_{\lambda}(U_{\mu}) - \Chi_{\mu}(U_{\mu}) }
@@ -287,14 +287,15 @@ use std::collections::HashMap;
 /// eval_proj_pairs reduces the product of projections in eq17 to a list of projections to add together, this is necessary as we do not have distributivity
 ///  and hence all need to be expanded out and computed as a sum of applications and scaling.
 /// v is a vector of pairs (usize, T) representing a term in the product of the form (C_n, \Chi_{n}(C_n)) representing (C_n - \Chi_{n}(C_n))
+/// unit is the representative of 1 in T
 /// returns the list of projections to apply and sum together
-fn eval_proj_pairs<T>(v: &[(usize, T)]) -> HashMap<Vec<usize>, T>
-where T: Mul<Output=T> + Add<Output=T> + Clone,
+fn eval_proj_pairs<T>(v: &[(usize, T)], unit: T) -> HashMap<Vec<usize>, T>
+where T: Mul<Output=T> + Add<Output=T> + Clone + Debug,
 {
     if v.len() == 0 { HashMap::new() }
-    else if v.len() == 1 { HashMap::from([ (vec![v[0].0], v[0].1.clone()) ]) }
+    else if v.len() == 1 { HashMap::from([ (vec![v[0].0], unit.clone()),  (vec![], v[0].1.clone()) ]) }
     else {
-        let before = eval_proj_pairs(&v[1..]);
+        let before = eval_proj_pairs(&v[1..], unit);
         let mut res: HashMap<Vec<usize>, T> = HashMap::new();
         for (vec,val) in before {
             if let Some(coeff) = res.get_mut(&vec) {
@@ -305,13 +306,66 @@ where T: Mul<Output=T> + Add<Output=T> + Clone,
             
             let vec_new = [vec, vec![v[0].0]].concat();
             if let Some(coeff) = res.get_mut(&vec_new) {
-                *coeff = coeff.clone() + v[0].1.clone() * val;
+                *coeff = coeff.clone() + val;
             } else {
-                res.insert(vec_new, v[0].1.clone()*val);
+                res.insert(vec_new, val);
             }
         }
         res
     }
+}
+
+/// apply_candidate_partitions_to_slp applies2 all candidate
+/// the projection is defined as P_{\lambda} = \Pi_{\mu \in \Lambda, \mu \neq \lambda} {
+///     \frac{ U_{\mu} - \Chi_{\mu}(U_{\mu}) }{ \Chi_{\lambda}(U_{\mu}) - \Chi_{\mu}(U_{\mu}) }
+/// }
+/// 
+/// when evaluated on a metapolynomial, U_{\mu} is applied to the metapolynomial, a scale of the metapoly by \Chi_{\mu}(U_{\mu}) is subtracted
+///  and the result is scaled by the denominator. All terms are then summed.
+/// 
+/// slp is the slp to project
+/// lambda is the corresponding integer partition to project the slp to
+/// partitions is a subset of all partitions of same length and integer as lambda alongside an index to a casimir element that distinguishes the two
+/// i64_to_c is a function converting integers into the coefficient object, it should be sensible wrt numerical operations and default, i64_to_c(0) = 0
+/// unit should be the multiplicative identity of the vector space being used for T
+/// returns a result of either the projected slp or an error message detailing the issue
+fn apply_candidate_partitions_to_slp2<T,F>(slp: SLP<T>, lambda: &Vec<u32>, partitions: &Vec<(Vec<u32>, usize)>, i64_to_c: F, unit: T) -> Result<SLP<T>, String>
+where 
+    T: Clone + Display + Default + Sub<Output=T> + Mul<Output=T> + Div<Output=T> + Add<Output=T> + Debug,
+    F: Fn(i64) -> T,
+{
+    // steps of finding the projection:
+    // 1. Find all candidate partitions (provided by partitions)
+    // 2. Expand out the product of the partition terms
+    // 3. Calculate the series of projections for each term in expanded product and scale appropriately
+    // 4. Sum the products and scale by the denominator product
+    if partitions.len() == 0 { return Err(String::from("No partitions found")); }
+    let k = dbg!(lambda.len());
+    if partitions.iter().any(|(v,_)| v.len() != k) { return Err(String::from("Partitions not all same length")); }
+
+    let terms: Vec<(usize, T)> = partitions.iter().map(|(v,i)| ( *i, i64_to_c(casimir_eigenval((*i) as u32, v, k)) )).collect();
+    let expanded = eval_proj_pairs(&terms.as_slice(), unit.clone());
+    let _ = dbg!(&expanded);
+
+    let mut summands = Vec::new();
+    for (proj_s, c) in expanded {
+        let mut slp_term = proj_s.into_iter().try_fold(slp.clone(),
+         |acc, proj| apply_casimir_to_slp(&acc, proj, k, |x| i64_to_c(x as i64))
+        )?;
+        scale_slp(&mut slp_term, c);
+        summands.push( slp_term );
+    }
+    if summands.len() == 0 { return Err("no partitions provided a projection".into()); }
+    let first = summands.first().unwrap().clone();
+    let mut slp_res = summands.into_iter().skip(1).fold(first, |acc, term| add_slp(acc, term));
+
+    let denom_scale = partitions.iter().map(
+        |(v,i)| i64_to_c(casimir_eigenval((*i) as u32, lambda, k)) - i64_to_c(casimir_eigenval((*i) as u32, v, k))
+    ).reduce(|acc,e| acc * e).ok_or("partitions is empty")?;
+
+    scale_slp( &mut slp_res,  unit / denom_scale );
+
+    Ok(slp_res)
 }
 
 #[cfg(test)]
@@ -400,12 +454,12 @@ mod tests {
 
         let mut res_str: String = String::new();
         File::open("example_5_4_proj.txt").unwrap().read_to_string(&mut res_str).unwrap();
-        let mut res_slp = slp_parser_rational(&res_str).unwrap();
+        let res_slp = slp_parser_rational(&res_str).unwrap();
         // scale_slp(&mut res_slp, Rational64::new(1,60) );
 
         let i64_to_c = |i| { Rational64::new(i,1) };
         let unit = Rational64::ONE;
-        let mut proj_slp = apply_candidate_partitions_to_slp(slp, &lambda, &distinguishing_parts, i64_to_c, unit).unwrap();
+        let mut proj_slp = apply_candidate_partitions_to_slp2(slp, &lambda, &distinguishing_parts, i64_to_c, unit).unwrap();
         scale_slp(&mut proj_slp, Rational64::new(60,1) );
         println!("{}", stepwise_slp_to_poly(&proj_slp, Rational64::ONE));
         println!("{}", stepwise_slp_to_poly(&res_slp, Rational64::ONE));
